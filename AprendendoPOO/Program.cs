5d995360f39a48dbfac7afde7f338afad39739cb
Marcio Costa <marcio-costa38@hotmail.com>
2025-05-18 22:34:24 -0300

    Adicionar arquivos de projeto.

---
+++ AprendendoPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AprendendoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caneta c1 = new Caneta();
+            c1.modelo = "BIC Cristal";
+            c1.cor = "Azul";
+            c1.ponta = 0.5f;
+            c1.carga = 80;
+            c1.tampada = true;
+
+            c1.status();
+
+            c1.destampar();
+
+            c1.status();
+        }
+    }
+}
---
+++ AprendendoPOO/usodeclasses/Caneta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoPOO
+{
+    internal class Caneta
+    {
+        public string cor;
+        public string modelo;
+        public float ponta;
+        public int carga;
+        public bool tampada;
+
+        public void status() 
+        {
+            Console.WriteLine("Modelo: " + this.modelo);
+            Console.WriteLine("Cor: " + this.cor); 
+            Console.WriteLine("Ponta: " + this.ponta);
+            Console.WriteLine("Carga: " + this.carga);
+            Console.WriteLine("Tampada: " + this.tampada);
+        }
+
+        public void rabiscar()
+        { 
+            if(this.tampada == true)
+            {
+                Console.WriteLine("Erro! Não posso rabiscar.");
+            }
+            else
+            {
+                Console.WriteLine("Estou rabiscando...");
+            }
+        }
+
+        public void tampar()
+        {
+            this.tampada = true;
+        }
+
+        public void destampar()
+        {
+            this.tampada = false;
+        }
+    }
+}
